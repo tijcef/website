ba4e3969e8f594570806c6bd9515cec56e7aad6a
tijcef <tijwuncare@gmail.com>
2026-07-30 16:22:20 +0100

    General project updates and improvements

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -93,5 +93,8 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+  require("@tailwindcss/typography"),
+  require("tailwindcss-animate"),
+],
 } satisfies Config;
